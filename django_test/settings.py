6e75250416aaebe196f4bcc36791ffe0e1ddc26c
Zephyr-2310 <zephyrcr72310@gmail.com>
2022-03-18 15:59:02 +0530

    modified settings

--- django_test/settings.py
+++ django_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+-af*e!wf&#v6n1@ix+gym3q1uv$78)2+25#-0#y*g1k0t)i@!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "djangotester23.herokuapp.com"]
 
 
 # Application definition
